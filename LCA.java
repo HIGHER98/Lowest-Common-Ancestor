fa4a4da5f762e7a8b8e38b3b15b06257d821f14e
HIGHER98 <hyerg@tcd.ie>
2018-10-02 15:00:44 +0100

    Add files via upload
    Commit

---
+++ LCA.java
@@ -0,0 +1,22 @@
+/*
+Lowest Common Ancestor	To implement a function that can calculate the lowest common ancestor in a graph, that may be structured as a binary tree. The grading focus is on your test code.
+*/
+public class LCA<Key>{
+
+  private Node<Key> root;
+    
+	private static class Node<Key>{
+    Key key;
+    Node<Key> left, right;
+
+    public Node(Key key){
+            this.key = key;
+            this.left = this.right = null;
+        }
+    }
+    
+    public static void main(String[] args) {
+    	
+    }
+}
+
