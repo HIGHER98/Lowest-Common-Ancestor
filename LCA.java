1ce72ad6bd5b1d24307ce76210a0707d94ee6e7f
HIGHER98 <hyerg@tcd.ie>
2018-10-02 17:38:14 +0100

    Update LCA.java
    Added methods to find lowest common ancestor

--- LCA.java
+++ LCA.java
@@ -1,12 +1,51 @@
 /*
-Lowest Common Ancestor	To implement a function that can calculate the lowest common ancestor in a graph, that may be structured as a binary tree. The grading focus is on your test code.
+Lowest Common Ancestor	
+To implement a function that can calculate the lowest common ancestor in a graph, that may be structured as a binary tree. 
+The grading focus is on your test code.
 */
 public class LCA<Key>{
 
   public Node<Key> root;
-    
-    public static void main(String[] args) {
-    	
+  
+    public Key lowestCommonAncestor(Key node0, Key node1){
+    Node<Key> lca = findLCA(root, node0, node1);
+
+    if(visited0 && visited1)
+      return lca.key;
+
+      return null;
     }
+
+  private Node<Key> findLCA(Node<Key> root, Key key0, Key key1) {
+
+    if (root == null)
+      return null;
+
+    Node<Key> temp = null;
+
+    if (root.key.equals(key0)) {
+     visited0 = true;
+     temp = root;
+    }
+    if (root.key.equals(key1)) {
+     visited1 = true;
+     temp = root;
+    }
+
+    Node<Key> left_lca = findLCA(root.left, key0, key1);
+    Node<Key> right_lca = findLCA(root.right, key0, key1);
+
+    if (temp != null)
+      return temp;
+
+    if (left_lca != null && right_lca != null)
+      return root;
+
+    return (left_lca != null) ? left_lca : right_lca;
+  }
+    
+  public static void main(String[] args) {
+    	
+  }
 }
 
